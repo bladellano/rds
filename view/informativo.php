45c84e5d5a01cbecb5ff8a51ce00fe011c63bf99
Caio Silva <bladellano@gmail.com>
2020-02-07 09:45:22 -0300

    Ajuste no informativo, unidades acrescentada

--- view/informativo.php
+++ view/informativo.php
@@ -11,12 +11,31 @@
 
   <div class="my-3 p-3 bg-white rounded shadow-sm">
     <h3 class="pb-2 mb-0">Informativo</h3>
-   
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur enim quae commodi porro quisquam tempore ex velit facilis assumenda expedita reprehenderit laboriosam perferendis, excepturi nulla? Dicta, incidunt! Ipsam impedit, dolore.
-    </p>
+    <hr>
+    <h3 class="pb-2 mb-0">Calendário Anual</h3>
+    <p></p>
+
+    <b>UNIDADE AUGUSTO MONTENGRO</b>
     <p>
-      &raquo; <a target="_blank" href="assets/CALENDARIO_ANUAL_DO_COLEGIO_ROSA_DE_SARON-AUGUSTO_MONTENEGRO_2020.pdf">CALENDÁRIO ANUAL DO COLÉGIO ROSA DE SARON <i class="fas fa-file-pdf"></i></a>
-    </p>        
+      <!-- &raquo; <a target="_blank" href="assets/CALENDARIO_ANUAL_DO_COLEGIO_ROSA_DE_SARON-AUGUSTO_MONTENEGRO_2020.pdf">CALENDÁRIO ANUAL DO COLÉGIO ROSA DE SARON <i class="fas fa-file-pdf"></i></a> -->
+      &raquo; <a target="_blank" href="#">CALENDÁRIO ANUAL DO COLÉGIO ROSA DE SARON <i class="fas fa-file-pdf"></i></a>
+    </p> <hr> 
+    <!--FIM-->
+    <b>UNIDADE TENONÉ</b>
+    <p>
+      &raquo; <a target="_blank" href="#">CALENDÁRIO ANUAL DO COLÉGIO ROSA DE SARON <i class="fas fa-file-pdf"></i></a>
+    </p> <hr> 
+    <!--FIM-->   
+    <b>UNIDADE EDUARDO ANGELIM</b>
+    <p>
+      &raquo; <a target="_blank" href="#">CALENDÁRIO ANUAL DO COLÉGIO ROSA DE SARON <i class="fas fa-file-pdf"></i></a>
+    </p> <hr> 
+    <!--FIM-->   
+    <b>UNIDADE CIDADE NOVA</b>
+    <p>
+      &raquo; <a target="_blank" href="#">CALENDÁRIO ANUAL DO COLÉGIO ROSA DE SARON <i class="fas fa-file-pdf"></i></a>
+    </p>  
+    <!--FIM-->         
 
   </div>
 </div>
